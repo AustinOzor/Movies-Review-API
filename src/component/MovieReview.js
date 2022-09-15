cc1e38b8219d6ded6bbb552e2dbef007ef6d9416
Austin Ozor <augustineozor@gmail.com>
2022-09-15 14:00:01 +0300

    second commit

--- src/component/MovieReview.js
+++ src/component/MovieReview.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import GodFather from "../images/gf.png"
+import Gang from "../images/gsgf.png"
+import GodFather3 from "../images/ygf.png"
+import logo from "../images/logo.png"
+
 
 class MovieReview extends Component{
     constructor(props) {
@@ -26,14 +31,19 @@ class MovieReview extends Component{
             <>
                 <Container>
          
-                <h1 className="users">Movie Reviews With <br/> "Big" in the title that opened in the U.S. between 1980 and 1990</h1>
+                    <div className="users">
+                         <img src={logo} alt='God Father' className="logo-img"/>
+                        <h1 >Movie Reviews With <br /> "Big" in the title that opened in the U.S. between 1980 and 1990</h1>
+                       
+                </div>
             <Row className="section">
-                        <Col md={4}>
+                        <Col md={3}>
                             <div>
                             
                              {this.state.movies.map((item, index) => {
                               return (
                                   <div key={index} className="card--body">
+                                  <img src={GodFather} alt='God Father'/>
                                   <p>Title: {item.display_title}</p>
                                   <p>Headline: {item.headline}</p>
                                   <p>Summary: {item.summary_short}</p>
@@ -46,12 +56,13 @@ class MovieReview extends Component{
                                 })}
                                 </div>
                         </Col>
-                        <Col md={4}>
+                        <Col md={3}>
                             <div>
                             
                              {this.state.movies.map((item, index) => {
                               return (
                                   <div key={index} className="card--body">
+                                  <img src={Gang} alt='God Father'/>
                                   <p>Title: {item.display_title}</p>
                                   <p>Headline: {item.headline}</p>
                                   <p>Summary: {item.summary_short}</p>
@@ -64,12 +75,31 @@ class MovieReview extends Component{
                                 })}
                                 </div>
                         </Col>
-                        <Col md={4}>
+                        <Col md={3}>
                             <div>
                             
                              {this.state.movies.map((item, index) => {
                               return (
                                   <div key={index} className="card--body">
+                                  <img src={GodFather3} alt='God Father'/>
+                                  <p>Title: {item.display_title}</p>
+                                  <p>Headline: {item.headline}</p>
+                                  <p>Summary: {item.summary_short}</p>
+                                  <p>Critics: {item.critics_pick}</p>
+                                  <p>Date: {item.date_updated}</p>
+                                  <p>By Line: {item.byline}</p>
+                                  </div>
+                              )
+                                })}
+                                </div>
+                        </Col>
+                        <Col md={3}>
+                            <div>
+                            
+                             {this.state.movies.map((item, index) => {
+                              return (
+                                  <div key={index} className="card--body">
+                                  <img src={GodFather3} alt='God Father'/>
                                   <p>Title: {item.display_title}</p>
                                   <p>Headline: {item.headline}</p>
                                   <p>Summary: {item.summary_short}</p>
